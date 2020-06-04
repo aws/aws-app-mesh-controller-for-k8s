9b2d3a59dcbe3c39f71bc03cab067f934b03711d
Brian Celenza <bcelenza@gmail.com>
2020-06-04 15:10:50 -0700

    Allow controller to optionally use App Mesh preview environment (#255)
    Co-authored-by: Brian Celenza <celenzab@amazon.com>

--- appmesh_models_override/setup.sh
+++ appmesh_models_override/setup.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+set -e
+
 mkdir -p ./vendor/github.com/aws
 
-git clone --depth 1 https://github.com/aws/aws-sdk-go.git ./vendor/github.com/aws/aws-sdk-go/
-API_PATH=./vendor/github.com/aws/aws-sdk-go/models/apis/appmesh/2019-01-25
-cp appmesh_models_override/api-2.json $API_PATH/api-2.json
+SDK_VENDOR_PATH=./vendor/github.com/aws/aws-sdk-go
+rm -rf $SDK_VENDOR_PATH
+git clone --depth 1 https://github.com/aws/aws-sdk-go.git $SDK_VENDOR_PATH
+API_PATH=$SDK_VENDOR_PATH/models/apis/appmesh/2019-01-25
+SERVICE_NAME=$([ "$APPMESH_PREVIEW" == "1" ] && echo "appmesh-preview" || echo "appmesh" )
+cat appmesh_models_override/api-2.json | jq "(.metadata | .endpointPrefix?, .signingName?) |= \"$SERVICE_NAME\"" > $API_PATH/api-2.json
 cp appmesh_models_override/docs-2.json $API_PATH/docs-2.json
 cp appmesh_models_override/examples-1.json $API_PATH/examples-1.json
 cp appmesh_models_override/paginators-1.json $API_PATH/paginators-1.json
