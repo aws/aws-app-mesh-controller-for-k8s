8e021c3923f9ce5532a3695454407f76d132d7d2
Yang Yang <yyyng@amazon.com>
2020-07-09 15:30:59 -0700

    partially fix the crash in issue 305

--- controllers/appmesh/cloudmap_controller.go
+++ controllers/appmesh/cloudmap_controller.go
@@ -97,8 +97,12 @@ func (r *cloudMapReconciler) reconcileVirtualNodeWithCloudMap(ctx context.Contex
 
 func (r *cloudMapReconciler) cleanupCloudMapResources(ctx context.Context, vNode *appmesh.VirtualNode) error {
 	if k8s.HasFinalizer(vNode, k8s.FinalizerAWSCloudMapResources) {
-		if err := r.cloudMapResourceManager.Cleanup(ctx, vNode); err != nil {
-			return err
+		// TODO: choose one of the approach in https://github.com/aws/aws-app-mesh-controller-for-k8s/issues/305#issuecomment-654374069
+		// to completely fix the issue
+		if vNode.Spec.ServiceDiscovery != nil && vNode.Spec.ServiceDiscovery.AWSCloudMap != nil {
+			if err := r.cloudMapResourceManager.Cleanup(ctx, vNode); err != nil {
+				return err
+			}
 		}
 		if err := r.finalizerManager.RemoveFinalizers(ctx, vNode, k8s.FinalizerAWSCloudMapResources); err != nil {
 			return err
