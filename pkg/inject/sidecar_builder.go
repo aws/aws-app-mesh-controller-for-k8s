fa911c740ffcf835a44a4ef71346b152308ffc41
Fawad Khaliq <fawadkh@amazon.com>
2020-08-04 15:58:45 -0700

    Add optional resource limits for init and sidecar containers (#326)

--- pkg/inject/sidecar_builder.go
+++ pkg/inject/sidecar_builder.go
@@ -2,6 +2,7 @@ package inject
 
 import (
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
 )
 
 func envoyReadinessProbe(initialDelaySeconds int32, periodSeconds int32) *corev1.Probe {
@@ -19,7 +20,7 @@ func envoyReadinessProbe(initialDelaySeconds int32, periodSeconds int32) *corev1
 			//  }
 			// server_info->state supports the following states: LIVE, DRAINING, PRE_INITIALIZING, and INITIALIZING
 			// LIVE: Server is live and serving traffic
-			// DRAINING:  ⁣Server is draining listeners in response to external health checks failing
+			// DRAINING: Server is draining listeners in response to external health checks failing
 			// PRE_INITIALIZING: Server has not yet completed cluster manager initialization
 			// INITIALIZING: Server is running the cluster manager initialization callbacks
 			Exec: &corev1.ExecAction{Command: []string{
@@ -45,4 +46,56 @@ func envoyReadinessProbe(initialDelaySeconds int32, periodSeconds int32) *corev1
 		// Keeping the failure threshold to 3 to not fail preemptively
 		FailureThreshold: 3,
 	}
+}
+
+func sidecarResources(cpuRequest, memoryRequest, cpuLimit, memoryLimit string) (corev1.ResourceRequirements, error) {
+	resources := corev1.ResourceRequirements{}
+
+	if cpuRequest != "" || memoryRequest != "" {
+		requests := corev1.ResourceList{}
+
+		if cpuRequest != "" {
+			cr, err := resource.ParseQuantity(cpuRequest)
+			if err != nil {
+				return resources, err
+			}
+			requests["cpu"] = cr
+		}
+
+		if memoryRequest != "" {
+			mr, err := resource.ParseQuantity(memoryRequest)
+			if err != nil {
+				return resources, err
+			}
+			requests["memory"] = mr
+		}
+
+		resources.Requests = requests
+
+	}
+
+	if cpuLimit != "" || memoryLimit != "" {
+		limits := corev1.ResourceList{}
+
+		if cpuLimit != "" {
+			cl, err := resource.ParseQuantity(cpuLimit)
+			if err != nil {
+				return resources, err
+			}
+			limits["cpu"] = cl
+		}
+
+		if memoryLimit != "" {
+			ml, err := resource.ParseQuantity(memoryLimit)
+			if err != nil {
+				return resources, err
+			}
+			limits["memory"] = ml
+		}
+
+		resources.Limits = limits
+
+	}
+
+	return resources, nil
 }
