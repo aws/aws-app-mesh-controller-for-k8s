1098a426e72c6f00ff080c015892fc2b739789bc
Apurup Chevuru <achevuru@amazon.com>
2020-08-07 11:56:38 -0700

    Support to configure DNS TTL value of CloudMap services

---
+++ pkg/cloudmap/config.go
@@ -0,0 +1,27 @@
+package cloudmap
+
+import (
+	"github.com/spf13/pflag"
+)
+
+const (
+	flagSetCloudMapTTL = "cloudmap-ttl"
+)
+
+type Config struct {
+	//Specifies the DNS TTL value to be used while creating CloudMap services.
+	CloudMapServiceTTL int64
+}
+
+func (cfg *Config) BindFlags(fs *pflag.FlagSet) {
+	fs.Int64Var(&cfg.CloudMapServiceTTL, flagSetCloudMapTTL, defaultServiceDNSConfigTTL,
+		`CloudMap Service DNS TTL value`)
+}
+
+func (cfg *Config) BindEnv() error {
+	return nil
+}
+
+func (cfg *Config) Validate() error {
+	return nil
+}
