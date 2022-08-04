99b25cb5b9082d9c55a30bfe956db502b30d3428
Vishal Athreya <bvishal.athreya@gmail.com>
2022-08-04 13:12:36 -0700

    Modify test/e2e/fishapp to handle load testing (#617)
    * Add fishapp test functionality in separate runnable script
    
    * Add createConfigMap to mount request_handler.py in containers
    update Deployment creation to ignore Egress port 443
    update core_resources.go to handle ConfigMap change
    
    * Add certs/ folder and scripts to load/
    add resource parameter fetch from config file
    fix spin up and spin down of resources
    
    * Add support to create fishapp resources from config
        * Update createResourcesForNodesWithTLS and related functions to use
          DynamicStackNew
    Add support to run Fortio after spin up of resources
        * Deploy Fortio components using yaml
        * Run Load Driver python script
    
    * Update deployFortio to fetch resource status from k8s
    instead of time.Sleep
    
    * Fix base-path cli flag parse error
    
    * Add services to Fortio Backend
    
    * Remove sleep used for testing purpose
    
    Co-authored-by: Vishal Athreya Baskaran <eavishal@amazon.com>
    Co-authored-by: Ben Du <5668844+bendu@users.noreply.github.com>

---
+++ test/e2e/fishapp/load/certs/node_certs.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+# $1=<certificate name>
+generate_rsa_key() {
+  openssl genrsa -out $DIR/$1_key.pem 2048
+}
+
+# $1=<certificate_name>
+generate_cert_config() {
+  sed -e "s/\${services_domain}/${SERVICES_DOMAIN}/" $DIR/$1_cert.cfg > $DIR/$1_cert.cfg.generated
+}
+
+# $1=<certificate name> $2=<CA name>
+generate_x509_cert() {
+  generate_cert_config $1
+  openssl req -new -key $DIR/$1_key.pem -out $DIR/$1_cert.csr -config $DIR/$1_cert.cfg.generated -batch -sha256
+  openssl x509 -req -days 3650 -in $DIR/$1_cert.csr -sha256 -CA $DIR/$2_cert.pem -CAkey \
+    $DIR/$2_key.pem -CAcreateserial -out $DIR/$1_cert.pem -extensions v3_req -extfile $DIR/$1_cert.cfg.generated
+}
+
+# $1=<certificate name> $2=<CA name>
+generate_cert_chain() {
+  cat $DIR/$1_cert.pem $DIR/$2_cert.pem > $DIR/$1_cert_chain.pem
+}
+
+# $1=<CA name> $2=<CA name>
+generate_cert_bundle() {
+  cat $DIR/$1_cert.pem $DIR/$2_cert.pem > $DIR/$1_$2_bundle.pem
+}
+
+# Generate RSA certs for the Virtual Node passed in via $1
+echo "Generating backend certificate."
+generate_rsa_key $1 ca
+generate_x509_cert $1 ca
+generate_cert_chain $1 ca
+
+rm $DIR/*.csr
