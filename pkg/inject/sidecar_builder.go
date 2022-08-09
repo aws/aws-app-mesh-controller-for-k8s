fc71833bfddc54982eb9a66725ac6159966f0077
Kwangu Paul Yeo <paulyeo@amazon.com>
2022-08-09 20:27:22 +0000

    Regex version to 1.22.2-appmesh.1

--- pkg/inject/sidecar_builder.go
+++ pkg/inject/sidecar_builder.go
@@ -198,10 +198,10 @@ func buildEnvoySidecar(vars EnvoyTemplateVariables, env map[string]string) (core
 	if vars.WaitUntilProxyReady {
 		envoy.Lifecycle.PostStart = &corev1.Handler{
 			Exec: &corev1.ExecAction{Command: []string{
-				// "sh", "-c", fmt.Sprintf("if [[ $(/usr/bin/envoy --version) =~ ([0-9]+\\.([0-9]+) && ${BASH_REMATCH[1]} -gt 2 || ${BASH_REMATCH[2]} -gt 22 ]];"+
-				// 	"then APPNET_AGENT_POLL_ENVOY_READINESS_TIMEOUT_S=%d APPNET_AGENT_POLL_ENVOY_READINESS_INTERVAL_S=%d /usr/bin/agent -envoyReadiness;"+
-				// 	"else echo 'WaitUntilProxyReady is not supported in Envoy < 1.22'; fi", vars.PostStartTimeout, vars.PostStartInterval),
-				"sh", "-c", "APPNET_AGENT_POLL_ENVOY_READINESS_TIMEOUT_S=60 APPNET_AGENT_POLL_ENVOY_READINESS_INTERVAL_S=5 /usr/bin/agent -envoyReadiness",
+				"sh", "-c", fmt.Sprintf("if [[ $(/usr/bin/envoy --version) =~ ([0-9]+)\\.([0-9]+)\\.([0-9]+)-appmesh\\.([0-9]+) && "+
+					"${BASH_REMATCH[1]} -gt 2 || ${BASH_REMATCH[2]} -gt 22 || (${BASH_REMATCH[3]} -eq 2 && ${BASH_REMATCH[4]} -gt 0) ]]; "+
+					"then APPNET_AGENT_POLL_ENVOY_READINESS_TIMEOUT_S=%d APPNET_AGENT_POLL_ENVOY_READINESS_INTERVAL_S=%d /usr/bin/agent "+
+					"-envoyReadiness; else echo 'WaitUntilProxyReady is not supported in Envoy version < 1.22.2.1'; fi", vars.PostStartTimeout, vars.PostStartInterval),
 			}},
 		}
 	}
