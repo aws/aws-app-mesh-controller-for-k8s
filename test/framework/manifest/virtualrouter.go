97e4b660e6988274cca721d7156d9f9ac25ec476
dileepng <114957777+dileepng@users.noreply.github.com>
2023-08-09 09:50:44 -0700

    adding test cases for TcpRouteMatch (#720)

--- test/framework/manifest/virtualrouter.go
+++ test/framework/manifest/virtualrouter.go
@@ -14,6 +14,13 @@ type RouteToWeightedVirtualNodes struct {
 	WeightedTargets []WeightedVirtualNode
 }
 
+type TcpRouteToWeightedVirtualNodes struct {
+	Match           *TCPRouteMatch
+	WeightedTargets []WeightedVirtualNode
+}
+type TCPRouteMatch struct {
+	Port *int64
+}
 type WeightedVirtualNode struct {
 	VirtualNode types.NamespacedName
 	Weight      int64
@@ -68,6 +75,34 @@ func (b *VRBuilder) BuildRoutes(routeCfgs []RouteToWeightedVirtualNodes) []appme
 	return routes
 
 }
+func (b *VRBuilder) TcpBuildRoutes(tcpRoutesCFgs []TcpRouteToWeightedVirtualNodes) []appmesh.Route {
+
+	var routes []appmesh.Route
+	for index, tcpRouteCfg := range tcpRoutesCFgs {
+		var targets []appmesh.WeightedTarget
+		for _, weightedTarget := range tcpRouteCfg.WeightedTargets {
+			targets = append(targets, appmesh.WeightedTarget{
+				VirtualNodeRef: &appmesh.VirtualNodeReference{
+					Namespace: aws.String(weightedTarget.VirtualNode.Namespace),
+					Name:      weightedTarget.VirtualNode.Name,
+				},
+				Weight: weightedTarget.Weight,
+			})
+		}
+		//match:= if tcpRouteCfg.
+		routes = append(routes, appmesh.Route{
+			Name: fmt.Sprintf("route-%d", index),
+			TCPRoute: &appmesh.TCPRoute{
+				Match: (*appmesh.TCPRouteMatch)(tcpRouteCfg.Match),
+				Action: appmesh.TCPRouteAction{
+					WeightedTargets: targets,
+				},
+				Timeout: nil,
+			},
+		})
+	}
+	return routes
+}
 
 func (b *VRBuilder) BuildVirtualRouterListener(protocol appmesh.PortProtocol, port appmesh.PortNumber) appmesh.VirtualRouterListener {
 	return appmesh.VirtualRouterListener{
@@ -75,6 +110,12 @@ func (b *VRBuilder) BuildVirtualRouterListener(protocol appmesh.PortProtocol, po
 			Port:     port,
 			Protocol: protocol,
 		},
+	}
+}
+
+func (b *VRBuilder) BuildTcpRouteMatch(port *int64) *TCPRouteMatch {
+	return &TCPRouteMatch{
+		Port: port,
 	}
 }
 
